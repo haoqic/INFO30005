e5d66952f1136a3bcc4d30ee2aa6062a8d66b47a
haoqic <57283626+haoqic@users.noreply.github.com>
2020-04-16 01:38:35 +1000

    Update app.js

--- app.js
+++ app.js
@@ -15,14 +15,13 @@ var app = express();
 
 
 // Set up mongoose connection
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://haoqic:haoqic@incubeta-wowel.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb+srv://haoqic:haoqic@incubeta-wowel.mongodb.net/test?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 // view engine setup
