a11ae1af0367fee99e0459c5562b90d363c0c65e
haoqic <57283626+haoqic@users.noreply.github.com>
2020-04-16 00:41:07 +1000

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://haoqic:haoqic@incubeta-wowel.mongodb.net/test?retryWrites=true&w=majority'
+var dev_db_url = 'mongodb+srv://haoqic:8717192@incubeta-wowel.mongodb.net/test?retryWrites=true&w=majority'
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
